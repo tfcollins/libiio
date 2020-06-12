103b0754dedb2f7cfcfd851573274ec596f05d72
Travis F. Collins <travis.collins@analog.com>
2020-06-12 10:36:25 -0700

    Add other architectures
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/azure/ci-debian-stretch-arm64.sh
+++ CI/azure/ci-debian-stretch-arm64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 uname -a
-apt update
-apt -y upgrade
-apt install -y bison flex cmake git build-essential libxml2-dev doxygen
-apt install -y python3 python3-sphinx python3-setuptools
+DEBIAN_FRONTEND=noninteractive apt update
+DEBIAN_FRONTEND=noninteractive apt -y upgrade
+DEBIAN_FRONTEND=noninteractive apt install -y bison flex cmake git build-essential libxml2-dev doxygen
+DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-sphinx python3-setuptools
 mkdir build && cd build
 cmake .. -DPYTHON_BINDINGS=ON
 make
