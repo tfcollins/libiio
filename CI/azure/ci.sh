6273169981d4a1bb613efd97dd8ffc29d6ad3c49
Travis F. Collins <travis.collins@analog.com>
2020-06-11 21:21:18 -0700

    Update azure
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/azure/ci.sh
+++ CI/azure/ci.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-uname -a
-whoami
-ls
-pwd
+set -x
+apt update
+apt upgrade -qq
+apt install bison flex cmake git build-essential libxml2-dev doxygen dot
+apt install python3 python3-sphinx
+mkdir build && cd build
+cmake .. -DPYTHON_BINDINGS=ON
+make
